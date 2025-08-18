625b128e469c28be403ccb62a2bda491925fe985
Truong Nguyen <nguyentritruong2005@gmail.com>
2025-08-18 14:33:56 +0700

    Add folder baiTapMayTinh

---
+++ baiTapMayTinh/baiTapMayTinh.js
@@ -0,0 +1,27 @@
+document.getElementById("plus").addEventListener("click", function() {
+    let soA = parseFloat(document.getElementById("so1").value);
+    let soB = parseFloat(document.getElementById("so2").value);
+    let ketQua = soA + soB;
+    document.getElementById("ketQua").value = ketQua.toFixed(2);
+})
+
+document.getElementById("minus").addEventListener("click", function() {
+    let soA = parseFloat(document.getElementById("so1").value);
+    let soB = parseFloat(document.getElementById("so2").value);
+    let ketQua = soA - soB;
+    document.getElementById("ketQua").value = ketQua.toFixed(2);
+})
+
+document.getElementById("multi").addEventListener("click", function() {
+    let soA = parseFloat(document.getElementById("so1").value);
+    let soB = parseFloat(document.getElementById("so2").value);
+    let ketQua = soA * soB;
+    document.getElementById("ketQua").value = ketQua.toFixed(2);
+})
+
+document.getElementById("divide").addEventListener("click", function() {
+    let soA = parseFloat(document.getElementById("so1").value);
+    let soB = parseFloat(document.getElementById("so2").value);
+    let ketQua = soA / soB;
+    document.getElementById("ketQua").value = ketQua.toFixed(2);
+})
