1d99648a62cf263b3b7aa21a15f01de2e6c4a11d
Gnourt108 <23115053122146@sv.ute.udn.vn>
2025-08-18 19:18:08 +0700

    Update baiTapMayTinh.js

--- baiTapMayTinh.js
+++ baiTapMayTinh.js
@@ -22,6 +22,11 @@ document.getElementById("multi").addEventListener("click", function() {
 document.getElementById("divide").addEventListener("click", function() {
     let soA = parseFloat(document.getElementById("so1").value);
     let soB = parseFloat(document.getElementById("so2").value);
-    let ketQua = soA / soB;
-    document.getElementById("ketQua").value = ketQua.toFixed(2);
+    if(soB === 0){
+        alert("Số bị chia phải khác 0")
+    }else{
+        let ketQua = soA / soB;
+        document.getElementById("ketQua").value = ketQua.toFixed(2);
+    }
+
 })
