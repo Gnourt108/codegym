f2aea69ce0e4a171e79037ee3411d5f8eea0ba53
Gnourt108 <23115053122146@sv.ute.udn.vn>
2025-08-18 19:14:56 +0700

    Update ungDungChuyenDoiTienTe.js

--- ungDungChuyenDoiTienTe.js
+++ ungDungChuyenDoiTienTe.js
@@ -3,14 +3,22 @@ document.getElementById("trans").onclick = function () {
     let to = parseInt(document.getElementById("to").value);
     let money = parseFloat(document.getElementById("moneyBefore").value);
     let moneyBefore;
-    if(from === 0 && to === 1){
-        moneyBefore = money / 26.000;
+    if(money < 0){
+        alert("Số tiền không được âm")
+    }else{
+        if(from === 0 && to === 1){
+            moneyBefore = money / 26.000;
+        }
+        else if(from === 1 && to === 0){
+            moneyBefore = money * 26.000;
+        }
+        else if(money < 0){
+            alert("Số tiền không được âm");
+        }
+        else{
+            moneyBefore = money;
+        }
+        document.getElementById("moneyAfter").value = moneyBefore.toFixed(2);
     }
-    else if(from === 1 && to === 0){
-        moneyBefore = money * 26.000;
-    }
-    else{
-        moneyBefore = money;
-    }
-    document.getElementById("moneyAfter").value = moneyBefore.toFixed(2);
+
 }
