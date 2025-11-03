4b099a1d77740491acb00b010982bcdf0827d2f8
gnourt108 <23115053122146@sv.ute.udn.vn>
2025-11-03 14:43:00 +0700

    Add files via upload
    module2_ss10_DSA

---
+++ ss10_DSA/LinkedList/MyLinkedList.java
@@ -0,0 +1,68 @@
+package ss10_DSA.LinkedList;
+
+import org.w3c.dom.Node;
+
+public class MyLinkedList {
+    private Node head;
+    private int numNodes;
+
+    public MyLinkedList(Object data) {
+        head = new Node(data);
+        numNodes++;
+    }
+
+    private class Node{
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData(){
+            return this.data;
+        }
+    }
+
+    public void add(int index, Object data){
+        Node temp = head;
+        Node holder;
+
+        //Duyệt đến vị trí (index - 1)
+        for (int i = 0; i < index-1 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+
+        //holder giữ phần còn lại sau vị trí cần chèn
+        holder = temp.next;
+
+        temp.next = new Node(data);
+
+        temp.next.next = holder;
+
+        numNodes++;
+    }
+
+    public void addFirst(Object data){
+        Node temp = head;
+        head = new Node(data);
+        head.next = temp;
+        numNodes++;
+    }
+
+    public Node get(int index){
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
---
+++ ss10_DSA/LinkedList/TestMyLinkedList.java
@@ -0,0 +1,14 @@
+package ss10_DSA.LinkedList;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList(10);
+        linkedList.addFirst(11);
+        linkedList.addFirst(12);
+        linkedList.addFirst(13);
+
+        linkedList.add(4, 9);
+        linkedList.add(4, 9);
+        linkedList.printList();
+    }
+}
